049e9138bbe2b348536797fadb8d88b9596fb825
Lookas (Łukasz) Konieczny <lookasziebice@gmail.com>
2021-02-14 18:41:59 +0100

    Create entity for chick breed

---
+++ src/Entity/Breed.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BreedRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=BreedRepository::class)
+ */
+class Breed
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $breedType;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getBreedType(): ?string
+    {
+        return $this->breedType;
+    }
+
+    public function setBreedType(string $breedType): self
+    {
+        $this->breedType = $breedType;
+
+        return $this;
+    }
+}
