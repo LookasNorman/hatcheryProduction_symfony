5b6452b46f15cba71164075c638405c790b820d0
Lookas (Łukasz) Konieczny <lookasziebice@gmail.com>
2021-02-14 20:09:05 +0100

    Change path from eggs/delivery to eggs_delivery

--- src/Controller/EggsDeliveryController.php
+++ src/Controller/EggsDeliveryController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/eggs/delivery")
+ * @Route("/eggs_delivery")
  */
 class EggsDeliveryController extends AbstractController
 {
